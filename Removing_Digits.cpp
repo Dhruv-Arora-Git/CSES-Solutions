a48fa222f6b0ef690d5c7730a171b604bc47cea5
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2023-05-05 21:55:59 +0530

    iter dp

---
+++ Removing_Digits.cpp
@@ -0,0 +1,62 @@
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define mp make_pair
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+const int mod = 1'000'000'007;
+
+int main()
+{
+    
+    fastio;
+    int n;
+    cin >> n;
+    ll dp[n + 1];
+
+    dp[0] = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        string num = to_string(i);
+        int s = sz(num);
+        dp[i] = 1e9;
+        for (int j = 0; j < s; ++j)
+        {
+            int dig = num[j] - '0';
+            dp[i] = min(dp[i], 1 + dp[i - dig]);
+        }
+    }
+    cout << dp[n] << "\n";
+
+    return 0;
+}
