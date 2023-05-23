31dfb82b45f29151babe514e76e8769855291d94
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2023-05-23 22:53:16 +0530

    power of 2

---
+++ 1617_Bit_Strings.cpp
@@ -0,0 +1,53 @@
+/*
+ ___
+|   \  | |__    _,_           __     __
+| /\ \ | '_ \  | __| | | | |  \ \   / /
+| \/ / | | | | | |   | (_) |   \ \_/ /
+|___/  |_| |_| |_|   \ ___ /    \___/
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL), cout.tie(NULL), ios_base::sync_with_stdio(false);
+#define ll long long int
+#define ld long double
+#define mp make_pair
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << " = " << x << '\n'
+#define deb2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << '\n'
+
+// ----------------------------------------------
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll Lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+const int mod = 1'000'000'007;
+ll fastPower(ll base, ll power)
+{
+    ll ans = 1;
+    while (base > 0)
+    {
+        if ((power & 1) == 1)
+            ans *= base;
+        ans %= mod;
+        base = base * base;
+        power = power >> 1;
+    }
+    return ans;
+}
+// ----------------------------------------------
+
+int main()
+{
+    fastio;
+
+    int n;
+    cin >> n;
+    ll ans = 1;
+    for (int i = 0; i < n; i++)
+    {
+        ans *= 2;
+        ans %= mod;
+    }
+    cout << ans;
+    return 0;
+}
